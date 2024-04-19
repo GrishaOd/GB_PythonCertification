673707ff050ed2145b11f01abc4c8d3d8cb4f8ed
Grisha <odnokonnygrisha@gmail.com>
2024-04-19 20:33:25 +0300

    feat: added save notes

--- Notes.py
+++ Notes.py
@@ -9,3 +9,9 @@ def load_notes():
     if os.path.exists("notes.json"):
         with open("notes.json", "r") as file:
             notes = json.load(file)
+
+
+def save_notes():
+    with open("notes.json", "w") as file:
+        json.dump(notes, file, indent=4)
+
